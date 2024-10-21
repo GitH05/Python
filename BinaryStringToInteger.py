af12501f426f3cbbdcd0e71ecb45b2e3658ad745
Santosh Kumar Dhawal <162580448+GitH05@users.noreply.github.com>
2024-10-21 20:53:45 +0530

    convert the string binary["1111"] into integer.

---
+++ BinaryStringToInteger.py
@@ -0,0 +1,11 @@
+# type Casting:
+# convert binary string into integer:
+
+# str to int
+binary = "1111"
+integerValue = int(binary, base=2)
+print(integerValue)
+
+# str to float
+string="23"
+print(float(string))
